4fc655ec961d35076ea748c4aa9a09fe1f374944
victorzottmann <zottmann.victor@gmail.com>
2021-03-01 23:49:17 +1100

    begin styling of new book form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,25 @@
 class BooksController < ApplicationController
   before_action :set_book, only: %i[ show edit update destroy ]
 
-  # GET /books or /books.json
+
   def index
     @books = Book.all
   end
 
-  # GET /books/1 or /books/1.json
+ 
   def show
   end
 
-  # GET /books/new
+ 
   def new
     @book = Book.new
   end
 
-  # GET /books/1/edit
+  
   def edit
   end
 
-  # POST /books or /books.json
+ 
   def create
     @book = Book.new(book_params)
 
@@ -34,7 +34,7 @@ class BooksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /books/1 or /books/1.json
+
   def update
     respond_to do |format|
       if @book.update(book_params)
@@ -47,7 +47,7 @@ class BooksController < ApplicationController
     end
   end
 
-  # DELETE /books/1 or /books/1.json
+ 
   def destroy
     @book.destroy
     respond_to do |format|
@@ -57,12 +57,12 @@ class BooksController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    
     def set_book
       @book = Book.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
+    
     def book_params
       params.require(:book).permit(:title, :description, :edition, :pages, :publication_date, :format, :availability, :author_id, :publisher_id, :category_id)
     end
