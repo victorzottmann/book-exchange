f09141f6a7ec49af46795e74358b151ac373177b
victorzottmann <zottmann.victor@gmail.com>
2021-03-02 21:59:42 +1100

    implement search bar to find books by title

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :set_book, only: %i[ show edit update destroy ]
 
 
@@ -36,24 +37,19 @@ class BooksController < ApplicationController
 
 
   def update
-    respond_to do |format|
-      if @book.update(book_params)
-        format.html { redirect_to @book, notice: "Book was successfully updated." }
-        format.json { render :show, status: :ok, location: @book }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
-    end
+    @book.update(book_params)
+    redirect_to @book
   end
-
  
   def destroy
     @book.destroy
-    respond_to do |format|
-      format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to books_path
+  end
+
+  def search
+    @title = params[:title]
+    @books = Book.where "title like ?", "%#{@title}%"
+    # redirect_to book_path
   end
 
   private
@@ -62,7 +58,7 @@ class BooksController < ApplicationController
       @book = Book.find(params[:id])
     end
 
-    
+
     def book_params
       params.require(:book).permit(:title, :description, :edition, :pages, :publication_date, :format, :availability, :author_id, :publisher_id, :category_id)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "books/search", to: "books#search", as: "book_search"
+  
   resources :books
   
   root to: "home#index"
