338da9982ed56f0512135ee0ca70671a8b9b04c4
victorzottmann <zottmann.victor@gmail.com>
2021-03-08 23:08:20 +1100

    link dropdown categories menu to each category show page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
   devise_for :users
   
   get "books/search", to: "books#search", as: "book_search"
+
+  get "categories/1", to: "categories#show", as: "creative_arts"
+  get "categories/2", to: "categories#show", as: "design"
+  get "categories/3", to: "categories#show", as: "tech"
+  get "categories/4", to: "categories#show", as: "science"
   
   root to: "home#index"
 end
