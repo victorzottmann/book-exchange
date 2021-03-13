7976dc965e0aff11a4ad19fdbf9331def0d28194
victorzottmann <zottmann.victor@gmail.com>
2021-03-13 22:21:42 +1100

    update general features to display rentals instead of exchange

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_12_122432) do
+ActiveRecord::Schema.define(version: 2021_03_13_105151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2021_03_12_122432) do
     t.bigint "category_id", null: false
     t.bigint "user_id"
     t.integer "price"
+    t.boolean "available"
     t.index ["author_id"], name: "index_books_on_author_id"
     t.index ["category_id"], name: "index_books_on_category_id"
     t.index ["publisher_id"], name: "index_books_on_publisher_id"
